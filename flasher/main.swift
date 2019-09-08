0ceeead777787106ec80fb45bd96e1ac6ea744e5
Calle Englund <calle@discord.bofh.se>
2019-09-08 11:57:50 +0200

    Initial Commit

---
+++ flasher/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  flasher
+//
+//  Created by Calle Englund on 2019-09-08.
+//  Copyright © 2019 Calle Englund. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
