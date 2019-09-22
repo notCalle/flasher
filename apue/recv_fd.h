e081b1052a07c4f3a87c1b4f99a5f546b493731d
Calle Englund <calle@discord.bofh.se>
2019-09-22 13:47:44 +0200

    Extend FileHandle with receive à la APUE
    This extends the FileHandle type with a `socketPair` static method,
    and a `receiveFileHandle` method, wrapping `recv_fd` from "Advanced
    Programming in the Unix Environment".

---
+++ apue/recv_fd.h
@@ -0,0 +1,14 @@
+//
+//  recv_fd.h
+//  flasher
+//
+//  Created by Calle Englund on 2019-09-22.
+//  Copyright © 2019 Calle Englund. All rights reserved.
+//
+
+#ifndef recv_fd_h
+#define recv_fd_h
+
+int recv_fd(int fd);
+
+#endif /* recv_fd_h */
